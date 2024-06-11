a153872b00322b58804010849cf8e907f148c86c
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2024-06-11 13:46:56 +0800

    update : ActionOrder Step List One Char Demo (Part 5)

--- src/screens/ActionOrderSimulatorScreen.tsx
+++ src/screens/ActionOrderSimulatorScreen.tsx
@@ -158,19 +158,64 @@ export default function ActionOrderSimulatorScreen() {
         <View style={{ width: 24, backgroundColor: "#55555500" }}></View>
 
         {/** Right - Showing Flow and Simuated List */}
-        <View style={{ flex: 0.55, backgroundColor: "#0000002C" }}>
+        <View style={{ flex: 0.55, backgroundColor: "#00000000" }}>
           {/** Header */}
-          <View>
+          <View style={{ flexDirection: "row" }}>
             {/** All Steps */}
-            <View style={{ paddingLeft: 16, paddingRight: 16, paddingBottom:12}}>
-              <Text style={{ fontSize: 20, color: "#FFF" }}>所有流程</Text>
+            <View style={{ paddingLeft: 16, paddingBottom: 12, width: 130 }}>
+              <Text style={{ fontSize: 18, color: "#FFF" }}>所有流程</Text>
             </View>
 
+            {/** Times, Remaining Pts, Energy, Action Value */}
+            <View style={{ paddingLeft: 16, paddingRight: 16, paddingBottom: 12, flexDirection: "row", flex: 1 }}>
+              <Text style={{ fontSize: 18, maxWidth: 100, color: "#FFF", flex: 1, textAlign: "center" }}>次數</Text>
+              <Text style={{ fontSize: 18, maxWidth: 100, color: "#FFF", flex: 1, textAlign: "center" }}>戰績點</Text>
+              <Text style={{ fontSize: 18, maxWidth: 100, color: "#FFF", flex: 1, textAlign: "center" }}>能量</Text>
+              <Text style={{ fontSize: 18, maxWidth: 100, color: "#FFF", flex: 2, textAlign: "center" }}>累計行動值</Text>
 
+            </View>
           </View>
-          
+
           {/** Step List */}
-          <View style={{ backgroundColor: "#F3F9FF66", flex: 1, borderRadius: 8, borderTopRightRadius: 20 }}>
+          <View style={{ backgroundColor: "#F3F9FF66", flex: 1, borderRadius: 8, borderTopRightRadius: 20, padding: 8 }}>
+            <View style={{ flexDirection: "row" }}>
+              <View style={{ width: 140, flexDirection: "row" }}>
+                {/** Character Icon with BG */}
+                <LinearGradient
+                  style={{
+                    borderRadius: 60,
+                    overflow: "hidden",
+                    width: 32,
+                    height: 32,
+                  }}
+                  colors={["#905A52", "#C8A471"]
+                  }
+                >
+                  <Image cachePolicy="none"
+                    transition={200}
+                    style={{ width: 32, height: 32 }}
+                    source={require("../../assets/images/character_icon/jingliu_icon.webp")}
+                  />
+                </LinearGradient>
+
+                <View style={{ width: 12 }}></View>
+
+                {/** Basic ATK */}
+                <View style={{ justifyContent: "space-between", flexDirection: 'row', flex: 1  }}>
+                  <Text style={{ color: "#444444", backgroundColor: "#FFFFFFCC", width: 28, height: 32, fontSize: 16, textAlignVertical: "center", textAlign: "center", borderRadius: 4, paddingLeft:4, paddingRight:4 }}>B</Text>
+                  <Text style={{ color: "#FFFFFF", backgroundColor: "#00000033", width: 28, height: 32, fontSize: 16, textAlignVertical: "center", textAlign: "center", borderRadius: 4, paddingLeft:4, paddingRight:4 }}>S</Text>
+                  <Text style={{ color: "#FFFFFF", backgroundColor: "#00000033", width: 28, height: 32, fontSize: 16, textAlignVertical: "center", textAlign: "center", borderRadius: 4, paddingLeft:4, paddingRight:4 }}>U</Text>
+                </View>
+              </View>
+
+              <View style={{ justifyContent: "center", flexDirection: 'row', flex: 1 }}>
+                <Text style={{ color: "#FFFFFF", fontSize: 16, textAlignVertical: "center", textAlign: "center", flex: 1 }}>1</Text>
+                <Text style={{ color: "#FFFFFF", fontSize: 16, textAlignVertical: "center", textAlign: "center", flex: 1 }}>4</Text>
+                <Text style={{ color: "#FFFFFF", fontSize: 16, textAlignVertical: "center", textAlign: "center", flex: 1 }}>95</Text>
+                <Text style={{ color: "#FFFFFF", fontSize: 16, textAlignVertical: "center", textAlign: "center", flex: 2 }}>72.91</Text>
+              </View>
+
+            </View>
           </View>
         </View>
       </View>
